0d79650f794f1f84202cc534d841ece2afbae74e
张丹丹 <zdd87550@163.com>
2019-01-05 17:53:14 +0800

    no message

--- static/js/index.js
+++ static/js/index.js
@@ -6,4 +6,22 @@ $(function (){
         loop : true,
         autoplay: 5000,
     });
+    getDynamicMenuList();
 });
+
+
+//获取数据
+function getDynamicMenuList(){
+    $.ajax({
+        url: "json/dynamic.json",
+        type: "GET",
+        dataType: "json", 
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+        },
+        success: function(res) {
+            var html = template('content', res);
+            document.getElementById("technologyContainer").innerHTML = html;
+        }
+     })
+}
